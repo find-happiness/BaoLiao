79fb1453bf9a93d15270e0a2f47123e640ac3183
find-happiness <w564263331@gmail.com>
2016-04-05 16:03:48 +0800

    添加华龙网

--- app/src/main/java/com/happiness/baoliao/MainActivity.java
+++ app/src/main/java/com/happiness/baoliao/MainActivity.java
@@ -22,7 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
   private static final String TAG = "MainActivity";
   WebView webview;
+  WebView webview2;
   Subscription subscription;
+
+  Subscription subscription2;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -31,10 +34,11 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     webview = (WebView) this.findViewById(R.id.webview);
+    webview2 = (WebView) this.findViewById(R.id.webview2);
     initWebView();
 
     //webview.loadUrl("http://baoliao.cq.qq.com/pc/detail.html?id=412775");
-    subscription = Observable.interval(1000, 60000, TimeUnit.MILLISECONDS)
+    subscription = Observable.interval(1000, 10000, TimeUnit.MILLISECONDS)
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe(new Subscriber<Long>() {
           @Override public void onCompleted() {
@@ -46,7 +50,25 @@ public class MainActivity extends AppCompatActivity {
           }
 
           @Override public void onNext(Long aLong) {
+            //http://cqwz.cqnews.net/redirect-8a3a5874-d766-44d7-9662-4a9989c0556e.html
             webview.loadUrl("http://baoliao.cq.qq.com/pc/detail.html?id=412850");
+          }
+        });
+
+    subscription2 = Observable.interval(1000, 10300, TimeUnit.MILLISECONDS)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<Long>() {
+          @Override public void onCompleted() {
+
+          }
+
+          @Override public void onError(Throwable e) {
+            Log.e(TAG, "onError: " + e.toString());
+          }
+
+          @Override public void onNext(Long aLong) {
+            //http://cqwz.cqnews.net/redirect-8a3a5874-d766-44d7-9662-4a9989c0556e.html
+            webview2.loadUrl("http://cqwz.cqnews.net/redirect-8a3a5874-d766-44d7-9662-4a9989c0556e.html");
           }
         });
   }
@@ -59,6 +81,18 @@ public class MainActivity extends AppCompatActivity {
     webview.getSettings().setJavaScriptEnabled(true);
     webview.clearCache(true);
     webview.setWebViewClient(new WebViewClient() {
+      @Override public void onPageFinished(WebView view, String url) {
+
+      }
+    });
+
+    webview2.setVisibility(WebView.VISIBLE);
+    webview2.getSettings().setSupportZoom(false);
+    webview2.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+    webview2.getSettings().setBuiltInZoomControls(false);
+    webview2.getSettings().setJavaScriptEnabled(true);
+    webview2.clearCache(true);
+    webview2.setWebViewClient(new WebViewClient() {
       @Override public void onPageFinished(WebView view, String url) {
 
       }
@@ -77,6 +111,9 @@ public class MainActivity extends AppCompatActivity {
     super.onDestroy();
     if (subscription != null && subscription.isUnsubscribed()) {
       subscription.unsubscribe();
+    }
+    if (subscription2 != null && subscription2.isUnsubscribed()) {
+      subscription2.unsubscribe();
     }
   }
 }
